cea7940ee0ad5e1108025b4534e3c0907e15016e
Vladislav <vladbabienko@yandex.ru>
2023-02-20 00:25:31 +0300

    Решил

--- HomeWork lesson2/Program.cs
+++ HomeWork lesson2/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int z = n % 100;
+Console.WriteLine(z);
+int n1 = z / 10;
+Console.WriteLine(n1);

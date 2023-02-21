e125928207d6356d669113edc4181759e957b860
Vladislav <vladbabienko@yandex.ru>
2023-02-21 21:34:19 +0300

    Решил задачу 13

---
+++ task13/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 99 && n < 1000)
+{
+    int n1 = n % 10;
+    Console.WriteLine(n1);
+}
+else 
+    if (n < -99 && n > -1000)
+{   
+    int n2 = -n % 10;
+    Console.WriteLine(-n2);
+}       
+    else
+     Console.WriteLine("Число не является техзначным");
+
+
